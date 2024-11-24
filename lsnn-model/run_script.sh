96b77ea8225445b21de1c973631ce8c1301529e1
= <coltonfox2004@gmail.com>
2024-11-23 19:54:09 -0500

    Add num threads

--- lsnn-model/run_script.sh
+++ lsnn-model/run_script.sh
@@ -8,6 +8,9 @@
 #SBATCH --time=48:00:00
 
 # https://stackoverflow.com/questions/66805098/how-to-run-jobs-in-paralell-using-one-slurm-batch-script
+
+export OMP_NUM_THREADS=4
+
 srun --exclusive -n 1 -c 1 python -m tools.train_eval_all.py --dataset=ECG5000 --epochs=5 --is_all_combs=1 &
 srun --exclusive -n 1 -c 1 python -m tools.train_eval_all.py --dataset=FORDA --epochs=5 --is_all_combs=1 &
 srun --exclusive -n 1 -c 1 python -m tools.train_eval_all.py --dataset=FORDB --epochs=5 --is_all_combs=1 &
